29d59027d8ba02207f6d81691c0bbe826cf0ea4d
thanhlinh <thanhhlinhh@gmail.com>
2022-03-31 17:18:56 +0700

    lab 4

---
+++ BigSchool_1811063011_ThanhLinh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BigSchool_1811063011_ThanhLinh.MvcApplication" Language="C#" %>
---
+++ BigSchool_1811063011_ThanhLinh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_1811063011_ThanhLinh.Startup))]
+namespace BigSchool_1811063011_ThanhLinh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
